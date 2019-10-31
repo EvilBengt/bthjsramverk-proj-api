673547a367a8ac3ca5fad95209d901738a0ebe54
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-10-31 20:31:26 +0100

    Removed unnecessary try-catches and fixed body param name

--- routes/account.js
+++ routes/account.js
@@ -5,58 +5,42 @@ const accountModel = require("../src/accountModel");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const email = jwtModel.getEmail(req.headers["x-access-token"]);
-        const balance = await accountModel.getAccountBalance(email);
-        const funds = await accountModel.getOwnedFunds(email);
+    const email = jwtModel.getEmail(req.headers["x-access-token"]);
+    const balance = await accountModel.getAccountBalance(email);
+    const funds = await accountModel.getOwnedFunds(email);
 
-        res.json({
-            data: {
-                balance: balance,
-                funds: funds
-            }
-        });
-    } catch {
-        res.status(500).send();
-    }
+    res.json({
+        data: {
+            balance: balance,
+            funds: funds
+        }
+    });
 });
 
 router.post("/deposit", async (req, res) => {
-    try {
-        await accountModel.deposit(
-            req.body.amount,
-            jwtModel.getEmail(req.headers["x-access-token"])
-        );
-        res.status(201).send();
-    } catch {
-        res.status(500).send();
-    }
+    await accountModel.deposit(
+        req.body.amount,
+        jwtModel.getEmail(req.headers["x-access-token"])
+    );
+    res.status(201).send();
 });
 
 router.post("/invest", async (req, res) => {
-    try {
-        await accountModel.invest(
-            req.body.fund,
-            req.body.amount,
-            jwtModel.getEmail(req.headers["x-access-token"])
-        );
-        res.status(201).send();
-    } catch {
-        res.status(500).send();
-    }
+    await accountModel.invest(
+        req.body.fundName,
+        req.body.amount,
+        jwtModel.getEmail(req.headers["x-access-token"])
+    );
+    res.status(201).send();
 });
 
 router.post("/sell", async (req, res) => {
-    try {
-        await accountModel.sell(
-            req.body.fund,
-            req.body.amount,
-            jwtModel.getEmail(req.headers["x-access-token"])
-        );
-        res.status(201).send();
-    } catch {
-        res.status(500).send();
-    }
+    await accountModel.sell(
+        req.body.fundName,
+        req.body.amount,
+        jwtModel.getEmail(req.headers["x-access-token"])
+    );
+    res.status(201).send();
 });
 
 module.exports = router;
