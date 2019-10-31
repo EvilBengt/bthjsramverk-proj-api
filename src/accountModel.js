f8c2088278b9e814ed718446255ecdccb44a8f00
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-10-31 20:32:47 +0100

    Deposit, sell and invest functionality + fixes and tweaks

--- src/accountModel.js
+++ src/accountModel.js
@@ -1,30 +1,44 @@
-const db = require("./db").instance();
+const db = require("./db");
 const fundsModel = require("./fundsModel");
 
 const accountModel = {
     getAccountBalance: async (email) => {
-        return await db.get(`SELECT balance FROM users WHERE email = $email`, {
+        return (await db.instance().get(`SELECT balance FROM users WHERE email = $email`, {
             $email: email
-        });
+        })).balance;
     },
     getOwnedFunds: async (email) => {
-        return await db.all(`SELECT fund, amount FROM user_funds WHERE email = $email`, {
+        return await db.instance().all(`
+            SELECT name,
+                   long_name,
+                   amount
+              FROM user_funds
+              LEFT JOIN funds
+                ON user_funds.fund = funds.name
+             WHERE user = $email
+        `, {
             $email: email
         });
     },
     deposit: async (amount, email) => {
-        await db.run(`UPDATE users SET balance = balance + $amount WHERE email = $email`, {
+        await db.instance().run(`UPDATE users SET balance = balance + $amount WHERE email = $email`, {
             $amount: amount,
             $email: email
         });
     },
     invest: async (fundName, amount, email) => {
         const balance = await accountModel.getAccountBalance(email);
-        const fundValue = await fundsModel.getValue(fundName);
+        const fundValue = (await db.instance().get(`
+            SELECT value
+              FROM funds
+             WHERE name = $name
+        `, {
+            $name: fundName
+        })).value;
         const totalCost = fundValue * amount;
 
         if (balance >= totalCost) {
-            await db.run(`
+            await db.instance().run(`
                 INSERT INTO user_funds
                 VALUES ($email, $fund, $amount)
                 On CONFLICT(user, fund) DO UPDATE SET amount = amount + $amount
@@ -33,12 +47,13 @@ const accountModel = {
                 $fund: fundName,
                 $amount: amount
             });
+            await accountModel.deposit(-totalCost, email);
         } else {
             throw new Error("Insufficient account balance.");
         }
     },
     sell: async (fundName, amount, email) => {
-        const fundAmount = db.get(`
+        const fundAmount = (await db.instance().get(`
             SELECT amount
             FROM user_funds
             WHERE user = $email
@@ -46,18 +61,41 @@ const accountModel = {
         `, {
             $email: email,
             $fund: fundName
-        }).amount;
+        })).amount;
 
         if (fundAmount >= amount) {
-            db.run(`
-                UPDATE user_funds
-                SET amount = amount - $amount
-                WHERE user = $email
-                  AND fund = $fund
+            const fundValue = (await db.instance().get(`
+                SELECT value
+                FROM funds
+                WHERE name = $name
             `, {
-                $amount: amount,
-                $fund: fundName
-            });
+                $name: fundName
+            })).value;
+            const totalCost = fundValue * amount;
+
+            if (fundAmount > amount) {
+                await db.instance().run(`
+                    UPDATE user_funds
+                    SET amount = amount - $amount
+                    WHERE user = $email
+                      AND fund = $fund
+                `, {
+                    $amount: amount,
+                    $email: email,
+                    $fund: fundName
+                });
+                await accountModel.deposit(totalCost, email);
+            } else if (fundAmount === amount) {
+                await db.instance().run(`
+                    DELETE FROM user_funds
+                     WHERE user = $email
+                       AND fund = $fund
+                `, {
+                    $email: email,
+                    $fund: fundName
+                });
+            }
+            await accountModel.deposit(fundValue, email);
         }
     }
 };
